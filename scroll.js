7edde2c9960f3b3d7d491ca4b60a6fd0599aee76
Trey Jackson <jacksrf@gmail.com>
2015-01-06 13:51:03 -0500

    scrolling and links for steps 1-4

---
+++ scroll.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $('#repo').click(function(){
+    $('#one').ScrollTo();
+  })
+})
+
+$(document).ready(function(){
+  $('#github').click(function(){
+    $('#two').ScrollTo();
+  })
+})
+
+$(document).ready(function(){
+  $('#server').click(function(){
+    $('#three').ScrollTo();
+  })
+})
+
+$(document).ready(function(){
+  $('#client').click(function(){
+    $('#four').ScrollTo();
+  })
+})
+
+
+$(document).ready(function(){
+  $('#testing').click(function(){
+    $('#five').ScrollTo();
+  })
+})
+
+$(document).ready(function(){
+  $('#styling').click(function(){
+    $('#six').ScrollTo();
+  })
+})
+
+$(document).ready(function(){
+  $('#hostgin').click(function(){
+    $('#seven').ScrollTo();
+  })
+})
